efee70624a6be68edf4a3b0c904a889946063ea5
LeshMesh <ivlex37rus@gmail.com>
2022-04-11 21:23:37 +0300

    Add SystemProperty

--- src/test/java/guru/qa/OwnerWebDriverTest.java
+++ src/test/java/guru/qa/OwnerWebDriverTest.java
@@ -1,6 +1,8 @@
 package guru.qa;
 
 import guru.qa.config.WebDriverProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -8,11 +10,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OwnerWebDriverTest {
 
-    private WebDriver driver = new WebDriverProvider().get();
+    private WebDriver driver;
+
+    @BeforeEach
+    public void startDriver() {
+        driver = new WebDriverProvider().get();
+    }
 
     @Test
     public void testDuckDuckGoSearch() {
         assertEquals(driver.getTitle(), "DuckDuckGo — Максимум конфиденциальности, минимум усилий.");
+    }
+
+    @AfterEach
+    public void stopDriver() {
         driver.quit();
     }
 }
--- src/test/java/guru/qa/config/WebDriverConfig.java
+++ src/test/java/guru/qa/config/WebDriverConfig.java
@@ -1,22 +1,44 @@
 package guru.qa.config;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 
 public class WebDriverConfig {
 
     public boolean isRemote() {
-        return false;
+        String  isRemote = System.getProperty("isRemote");
+        if (Objects.isNull(isRemote)) {
+            return false;
+        }
+        return Boolean.parseBoolean(isRemote);
     }
 
     public Browser getBrowser() {
-        return Browser.FIREFOX;
+        String browser = System.getProperty("browser");
+        if (Objects.isNull(browser)) {
+            return Browser.CHROME;
+        }
+        return Browser.valueOf(browser);
     }
 
     public String getBaseUrl() {
-        return "https://duckduckgo.com";
+        String baseUrl = System.getProperty("baseUrl");
+        if (Objects.isNull(baseUrl)) {
+            baseUrl = "https://duckduckgo.com";
+        }
+        return baseUrl;
     }
 
     public URL isRemoteUrl() {
-        return null;
+        String remoteUrl = System.getProperty("remoteUrl");
+        if (Objects.isNull(remoteUrl)) {
+            return null;
+        }
+        try {
+            return new URL(remoteUrl);
+        } catch (MalformedURLException e) {
+            return null();
+        }
     }
 }
