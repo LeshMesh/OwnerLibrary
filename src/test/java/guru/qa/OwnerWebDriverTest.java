2cfd2274d749156087eed16cd9b4afa5ff0b502a
LeshMesh <ivlex37rus@gmail.com>
2022-04-11 21:03:21 +0300

    Add WebDriverConfig and change OwnerWebDriverTest

--- src/test/java/guru/qa/OwnerWebDriverTest.java
+++ src/test/java/guru/qa/OwnerWebDriverTest.java
@@ -1,17 +1,17 @@
 package guru.qa;
 
+import guru.qa.config.WebDriverProvider;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OwnerWebDriverTest {
 
+    private WebDriver driver = new WebDriverProvider().get();
+
     @Test
     public void testDuckDuckGoSearch() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://duckduckgo.com");
         assertEquals(driver.getTitle(), "DuckDuckGo — Максимум конфиденциальности, минимум усилий.");
         driver.quit();
     }
---
+++ src/test/java/guru/qa/config/WebDriverConfig.java
@@ -0,0 +1,2 @@
+package guru.qa.config;public class WebDriverConfig {
+}
